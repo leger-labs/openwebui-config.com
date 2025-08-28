241daa6aa15beed01b5ce822672a9e6a9962e0f3
mecattaf <thomas@mecattaf.dev>
2025-08-28 13:04:37 -0400

    chore: cleanup redundant original-schemas folder

--- src/schemas-original-output-from-gh-action/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * OpenWebUI Configuration Schemas
- * DO NOT EDIT DIRECTLY - Changes will be overwritten
- *
- * This file exports Zod schemas generated from the OpenAPI configuration.
- */
-export * from './generated-schemas';
